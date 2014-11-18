8de0ff1ae6be91c6a466fb3eea287684e634a839
Dmitry Votintsev <dvotintsev@gmail.com>
2014-11-18 01:04:47 -0500

    Install script adopted for RPi

--- install_modules_RPi.sh
+++ install_modules_RPi.sh
@@ -3,3 +3,7 @@ JasperLoc=`find / 2>/dev/null -type d -name 'jasper'`/client/modules
 cp RPI_Modules/*  $JasperLoc
 cp Shared_Modules/*  $JasperLoc
 echo Installed files in $JasperLoc
+sudo apt-get install python-serial
+sudo wget https://raw.githubusercontent.com/lurch/rpi-serial-console/master/rpi-serial-console -O /usr/bin/rpi-serial-console && sudo chmod +x /usr/bin/rpi-serial-console
+sudo rpi-serial-console disable
+echo Configured serial
