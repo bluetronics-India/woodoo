a196e91f767fca17bf6e437795e0d5088d1bd12c
unknown <wiizeldaguy@gmail.com>
2014-11-17 23:42:17 -0500

    Stuff

--- RPI_Modules/Wireless_Test.py
+++ RPI_Modules/Wireless_Test.py
@@ -1,15 +1,34 @@
-import RPi.GPIO as GPIO
 import re
 import os
+import serial
 
-WORDS = ["TOGGLE","LIGHT","ON","OFF"]
+WORDS = ["RED","GREEN","BLUE","ONE","ZERO"]
 
 def handle(text, mic, profile):
 
-	if(bool(re.search(r'\bON\b', text, re.IGNORECASE))):
-		os.call('screen /dev/AMA0 9600,cs8\nr1 r1r1r1')
+	if( bool(re.search(r'\bRED\b', text, re.IGNORECASE))):
+		txt = "r"
+	elif(bool(re.search(r'\bBLUE\b', text, re.IGNORECASE)) ):
+		txt="b"
+	elif(bool(re.search(r'\bGREEN\b', text, re.IGNORECASE)) ):
+		txt="g"
 	else:
-		os.call('screen /dev/AMA0 9600,cs8\n r0 r0r0r0')
+		break
+
+	if( bool(re.search(r'\bONE\b', text, re.IGNORECASE))):
+		e="1"
+	elif(bool(re.search(r'\bZERO\b', text, re.IGNORECASE)) ):
+		e="0"
+	else:
+		break
+
+	r = ""
+	for i in range(8):
+		r+=str(txt)+str(e)
+
+
+	ser = serial.Serial('/dev/ttyAMA0',9600)
+	ser.write(unicode(r)
 
 def isValid(text):
-    return bool(re.search(r'\bTOGGLE LIGHT\b', text, re.IGNORECASE)) or bool(re.search(r'\bTest\b', text, re.IGNORECASE))
+    return bool(re.search(r'\bRED\b', text, re.IGNORECASE)) or bool(re.search(r'\bBLUE\b', text, re.IGNORECASE)) or bool(re.search(r'\bGREEN\b', text, re.IGNORECASE))
